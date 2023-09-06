b06ebca363f8820a1fbc4108ac1506fbbeb9b303
Somala Prasanna Kumar <104211409+Prasannakumar55@users.noreply.github.com>
2023-09-06 16:53:29 +0530

    Add files via upload

---
+++ sorry.js
@@ -0,0 +1,15 @@
+function sorry (){
+    let count = 0; 
+    for (let i = 0; i < 100000; i++){
+       let total = (count+i);
+      
+       document.write(total + "." + "SORRY")
+       document.write("\n")
+    //    document.write("hi")
+
+       }
+}
+sorry();
+
+// function hi() {
+// 
